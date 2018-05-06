d21f286b07a7d97a05b0e9777aa4487265ec7874
Runcy Oommen <runcy.oommen@gmail.com>
2018-05-06 20:13:55 +0530

    Remove old genPP folder

--- enc_dec/genPP/make-include.mk
+++
@@ -1,5 +0,0 @@
-# Common definitions
-GPP = g++
-GPP_FLAGS = -Wall -g -std=c++1y
-
-LDFLAGS = -lssl -lcrypto
--- enc_dec/genPP/sha256.h
+++
@@ -1,6 +0,0 @@
-#ifndef SHA256_H_
-#define SHA256_H_
-
-string sha256(const string str);
-
-#endif /* SHA256_H_ */
