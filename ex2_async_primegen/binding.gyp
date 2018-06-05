7fb2ba55b2b8dbe3f4f93cd739680baee17be41a
Runcy Oommen <runcy.oommen@gmail.com>
2018-06-05 17:42:29 +0530

    Remove stale example

--- ex2_async_primegen/binding.gyp
+++
@@ -1,9 +0,0 @@
-# Define the targets to be created - primegen
-{
-	"targets": [
-	{
-		'target_name': 'primegen',
-		'sources': [ 'mainPrime.cpp', 'prime.cpp' ],
-	}
-	]
-}
--- ex2_async_primegen/index.js
+++
@@ -1,7 +0,0 @@
-var pg = require('./node_modules/primegen.node');
-
-exports.handler = function(event, context) {
-    pg.calcPrime(event["range"], function(results) {
-        context.succeed(results);
-    });
-};
